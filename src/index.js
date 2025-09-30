8f7e432421b2954b854019d4707a8da2b8b8a210
vaneorozco08 <vaneorozco8@gmail.com>
2025-09-29 19:42:14 -0500

    Update Weather API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    
+    cityElement.innerHTML = response.data.city; 
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city){
+    let apiKey ="a193eafba001c3fe70baa02c4to7f92d";
+    let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function cityWeather(event) {
   event.preventDefault();  
   let searchElement = document.querySelector("#search-input");
-  let cityElment = document.querySelector("#city");
-  cityElment.innerHTML = searchElement.value;  
+  
+   searchCity(searchElement.value);
 }
 
 
