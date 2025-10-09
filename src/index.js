a06e9385bceaf609b9a38691c743c1afa36aaa18
vaneorozco08 <vaneorozco8@gmail.com>
2025-10-09 18:01:45 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
     windElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
+
+    getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -41,10 +43,9 @@ function formatDate(date) {
 
 function searchCity(city){
     let apiKey ="a193eafba001c3fe70baa02c4to7f92d";
-    let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(refreshWeather);
 }
-
 
 function cityWeather(event) {
   event.preventDefault();  
@@ -52,7 +53,32 @@ function cityWeather(event) {
   
    searchCity(searchElement.value);
 }
+function getForecast(city){
+    let apiKey = "a193eafba001c3fe70baa02c4to7f92d"
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
+function displayForecast(response){
+    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+    let forecastHtml = "";
+}
+days.forEach(function (day){
+    forecastHtml =
+        forecastHtml +
+        `
+        <div class="weather-forescast-date">
+            <div class="weather-forecast-day">${day}</div> 
+            <div class="weather-forecast-icon">⛅</div> 
+            <div class="weather-forecast-temperature">
+                <div class="high-temp">19°</div> 
+                <div class="low-temp">15°</div>
+            </div>
+        </div>    
+        `;
+});
 
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit",cityWeather);
